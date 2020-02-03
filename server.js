a94f6cf5515b4ee6270826f0dbd47c6a84dbaff8
Renan Ciciliato <renanciciliato@gmail.com>
2020-02-03 13:30:48 -0200

    Conectando database / Criando model de produto

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
+// iniciando app
 const app = express();
 
+// iniciando db
+mongoose.connect(
+  'mongodb://192.168.99.100:27017/nodeapi', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  }
+);
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
+
+// primeira rota
 app.get('/', (req, res) => {
+  Product.create({
+    title: 'React Native',
+    description: 'Build native apps with React',
+    url: 'http://github.com/facebook/react-native'
+  });
   res.send('Hello Rocketseat');
 });
 
